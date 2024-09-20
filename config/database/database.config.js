db9ae417afe0cef942cbb428712db3c4e88f5819
sudheerreddy9999 <sudheerjanga9999@gmail.com>
2024-09-21 00:55:19 +0530

    db path

--- config/database/database.config.js
+++ config/database/database.config.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize({
   dialect: properties.get('db.pgsql.dialect'),
   host: properties.get('db.pgsql.host'),
   username: properties.get('db.pgsql.username'),
-  password: properties.get('db.pgsql.password'),
+  password: properties.get('db.pgsql.password') || process.env.DB_PASSWORD,
   database: properties.get('db.pgsql.database'),
   port: properties.get('db.pgsql.port'),
   dialectOptions: {
--- server.js
+++ server.js
@@ -11,8 +11,6 @@ import pgsql from './config/database/database.config.js';
 import Router from './routes/index.routes.js';
 import OpenApi from './utility/swagger.utility.js';
 import logger from './utility/logger.utility.js';
-import fs from 'fs'
-import path from 'path';
 
 const app = express();
 
@@ -37,23 +35,6 @@ const databaseConnection = async () => {
     logger.error(` database connection faild with ${error.message}`)
   }
 };
-
-
-// Specify the directory you want to check (replace 'your_directory_here' with the actual folder name)
-const directoryPath = path.join(process.cwd(), '../'); // Adjust if needed
-
-// Read the files in the directory
-fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-        console.error('Unable to scan directory:', err);
-        return;
-    }
-    // List all files in the directory
-    console.log('Files in directory:', directoryPath);
-    files.forEach(file => {
-        console.log(file);
-    });
-});
 
 const StartServer = () => {
   try {
