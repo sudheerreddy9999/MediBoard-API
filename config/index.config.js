e61518a31601a07115078def34e7a2b5dfc281d7
sudheerreddy9999 <sudheerjanga9999@gmail.com>
2024-09-21 00:08:30 +0530

    path change

--- config/index.config.js
+++ config/index.config.js
@@ -1,7 +1,9 @@
 'use strict';
 import PropertiesReader from 'properties-reader';
 
-const filePropertyPath = './config/env/config.properties';
+//let filePropertyPath = './config/env/config.properties';
+
+let filePropertyPath = '/etc/secrets/config.properties'
 
 const properties = PropertiesReader(filePropertyPath);
 
