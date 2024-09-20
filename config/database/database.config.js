594f3177ed61843356283b1bc729da636d4fa88c
sudheerreddy9999 <sudheerjanga9999@gmail.com>
2024-09-21 01:09:18 +0530

    db config change

--- config/database/database.config.js
+++ config/database/database.config.js
@@ -1,18 +1,19 @@
 'use strict';
 import properties from '../index.config.js';
 import { Sequelize } from 'sequelize';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const sequelize = new Sequelize({
-  dialect: properties.get('db.pgsql.dialect'),
-  host: properties.get('db.pgsql.host'),
-  username: properties.get('db.pgsql.username'),
-  password: properties.get('db.pgsql.password') || process.env.DB_PASSWORD,
-  database: properties.get('db.pgsql.database'),
-  port: properties.get('db.pgsql.port'),
+  dialect: properties.get('db.pgsql.dialect') || process.env.PGSQL_DIALECT,
+  host: properties.get('db.pgsql.host') || process.env.PGSQL_HOST,
+  username: properties.get('db.pgsql.username') || process.env.PGSQL_USERNAME,
+  password: properties.get('db.pgsql.password') || process.env.PGSQL_PASSWORD,
+  database: properties.get('db.pgsql.database') || process.env.PGSQL_DATABASE,
+  port: properties.get('db.pgsql.port') || process.env.PGSQL_PORT,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // This is typically needed if the server uses self-signed SSL certificates
+      rejectUnauthorized: false, // This is typically needed if the server uses self-signed SSL certificates
     },
   },
   pool: {
